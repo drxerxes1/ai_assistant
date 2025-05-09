7fa809f41eba7c9ae7d76c4a4a27633eedf8b914
drxerxes1 <davidroyxerxesomanio@gmail.com>
2025-05-10 02:36:08 +0800

    Animated Onboarding Screen 2

--- lib/screen/onboarding_screen.dart
+++ lib/screen/onboarding_screen.dart
@@ -2,84 +2,127 @@ import 'package:ai_assistant/helper/global.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
+import '../model/onboard.dart';
+import 'home_screen.dart';
+
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          // Lottie
-          Lottie.asset('assets/lottie/ai_ask_me.json', height: mq.height * .6),
+    final controller = PageController();
 
-          // Title
-
-          const Text(
-            'Ask me anything',
-            style: TextStyle(
-                fontSize: 18, fontWeight: FontWeight.bold, letterSpacing: .5),
-          ),
-
-          SizedBox(
-            height: mq.height * .01,
-          ),
-
-          // subtitle
-
-          SizedBox(
-            width: mq.width * .7,
-            child: const Text(
+    final list = [
+      Onboard(
+          title: 'Ask me anything',
+          subtitle:
               'I can be your personal assistant, just ask me anything & I will do my best to help you.',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 13.5, letterSpacing: .5, color: Colors.black54),
+          lottie: 'ai_ask_me'),
+      Onboard(
+          title: 'Imagination to Reality',
+          subtitle:
+              'I can help you to turn your imagination into reality, I will create a visual representation of your ideas.',
+          lottie: 'imagination'),
+      Onboard(
+          title: 'Translate languages',
+          subtitle:
+              'I can help you to translate languages, just ask me to translate any text to any language.',
+          lottie: 'translate'),
+    ];
+
+    return Scaffold(
+        body: PageView.builder(
+          controller: controller,
+      itemCount: list.length,
+      itemBuilder: (ctx, ind) {
+        final isLast = ind == list.length - 1;
+
+        return Column(
+          children: [
+            // Lottie
+            Lottie.asset('assets/lottie/${list[ind].lottie}.json',
+                height: mq.height * .6),
+
+            // Title
+
+            Text(
+              list[ind].title,
+              style: const TextStyle(
+                  fontSize: 18, fontWeight: FontWeight.bold, letterSpacing: .5),
             ),
-          ),
 
-          const Spacer(),
+            SizedBox(
+              height: mq.height * .01,
+            ),
 
-          // dots
+            // subtitle
 
-          Wrap(
-            spacing: 5,
-            children: List.generate(
-              3,
-              (index) => Container(
-                margin: const EdgeInsets.all(5),
-                width: 10,
-                height: 10,
-                decoration: const BoxDecoration(
-                  color: Colors.black54,
-                  shape: BoxShape.circle,
+            SizedBox(
+              width: mq.width * .7,
+              child: Text(
+                list[ind].subtitle,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                    fontSize: 13.5, letterSpacing: .5, color: Colors.black54),
+              ),
+            ),
+
+            const Spacer(),
+
+            // dots
+
+            Wrap(
+              spacing: 5,
+              children: List.generate(
+                list.length,
+                (index) => Container(
+                  margin: const EdgeInsets.all(5),
+                  width: index == ind ? 20 : 10,
+                  height: 10,
+                  decoration: BoxDecoration(
+                    color: index == ind ? Colors.blue : Colors.grey,
+                    borderRadius: const BorderRadius.all((Radius.circular(5))),
+                  ),
                 ),
               ),
             ),
-          ),
 
-          const Spacer(),
+            const Spacer(),
 
-          // button
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              shape: const StadiumBorder(),
-              textStyle:
-                  const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.white,
-              padding: EdgeInsets.symmetric(
-                  horizontal: mq.width * .2, vertical: mq.height * .02),
-              elevation: 0,
+            // button
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: const StadiumBorder(),
+                textStyle:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+                padding: EdgeInsets.symmetric(
+                    horizontal: mq.width * .2, vertical: mq.height * .02),
+                elevation: 0,
+              ),
+              onPressed: () {
+                if (isLast) {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (_) => const HomeScreen(),
+                    ),
+                  );
+                } else {
+                  controller.nextPage(
+                      duration: const Duration(milliseconds: 600),
+                      curve: Curves.ease);
+                }
+              },
+              child: Text(
+                isLast ? 'Finish' : 'Next',
+              ),
             ),
-            onPressed: () {},
-            child: const Text(
-              'Next',
-            ),
-          ),
 
-          const Spacer(),
-        ],
-      ),
-    );
+            const Spacer(),
+          ],
+        );
+      },
+    ));
   }
 }
